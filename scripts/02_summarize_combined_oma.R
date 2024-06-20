8ca30bb974241c9d5bc4e06f80e33f4179a1ebc5
Biplabendu Das <biplabendu.das@gmail.com>
2024-06-20 11:55:42 -0700

    feat: add combined oma file, summary script

---
+++ scripts/02_summarize_combined_oma.R
@@ -0,0 +1,75 @@
+library(dplyr)
+
+
+# read file ---------------------------------------------------------------
+
+dat <- read.csv(
+  "./ORTHOLOGY/result/combined_oma_v1.csv",
+  na.strings = c("", " ")
+) |> 
+  as_tibble()
+
+
+# summary 1: different combinations ---------------------------------------
+
+dat |> 
+  # mutate_all(as.factor) |> summary()
+  mutate(
+    across(
+      everything(),
+      ~ if_else(
+        !is.na(.),
+        "present",
+        "x"
+      )
+    )
+  ) |> 
+  group_by(
+    across(
+      everything()
+    )
+  ) |> 
+  tally(
+    name = "n_genes"
+  ) |> 
+  arrange(
+    desc(n_genes)
+  ) |> 
+  filter(
+    n_genes > 100
+  ) |> 
+  View()
+
+
+# summary 2: present in all -----------------------------------------------
+
+sub <- dat |> 
+  filter(
+    if_all(
+      everything(),
+      ~ !is.na(.)
+    )
+  )
+
+# For how many mouse genes, there are ≥ 2 genes in at least one more species?
+sub |> 
+  group_by(
+    mouse_ID
+  ) |> 
+  tally() |> 
+  mutate(
+    n = if_else(
+      n == 1,
+      "1:1",
+      "1:n possible"
+    )
+  ) |> 
+  group_by(
+    n
+  ) |> 
+  tally() |> 
+  rename(
+    orthology = n,
+    n_genes = nn
+  ) |> 
+  View()
