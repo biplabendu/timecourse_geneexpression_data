08285ba5f1669b1e6b8d0515df2ef31c44510f88
amanprasanna <abprasan@ncsu.edu>
2024-10-18 17:08:33 -0400

    feat: update documentation

--- R/analyze_network_topology.R
+++ R/analyze_network_topology.R
@@ -1,3 +1,16 @@
+#' @title 
+#' Perform network topology analysis on the data
+#' 
+#' @param data a single dataframe where columns are gene names(feature) and rows
+#' are time points
+#' 
+#' @return a list and a plot. a list of two components: 1.$powerEstimate (an 
+#' integer that is a recommended power) 2. $fitIndices (a dataframe that shows
+#' scale free topology model fit). The plot visualizes the Scale Independence 
+#' and Mean connectivity from the $fitIndices element of the list.
+#' 
+#'
+#' 
 analyze_network_topology <- function(data) {
   
   cat("Performing network topology analysis to pick 
